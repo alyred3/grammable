6d7164cabf4c24185cd52e51398b9efecf33accc
Alyssa Redman <alyred3@gmail.com>
2019-10-11 00:56:56 +0000

    Added comments

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,25 @@
 # config/initializers/carrierwave.rb
 
+#CarrierWave.configure do |config|
+#  config.fog_credentials = {
+#    provider: 'AWS',
+#    aws_access_key_id: ENV["AWS_ACCESS_KEY"],
+#    aws_secret_access_key: ENV["AWS_SECRET_KEY"],
+#    region: ENV["AWS_REGION"]
+#  }
+#  config.fog_directory  = ENV["AWS_BUCKET"]
+#end
+
 CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'                        # required
-  config.fog_credentials = {
-    provider:              'AWS',                        # required
-    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
-    aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
+ config.fog_credentials = {
+  provider:              'AWS',                        # required
+  aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
+  aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
   }
   config.fog_directory  = ENV["AWS_BUCKET"]              # required
+
+  if Rails.env.development?
+    config.cache_dir = '/home/vagrant/uploads_tmp/tmp/uploads'
+    config.root = '/home/vagrant/uploads_tmp/tmp'
+  end
 end
